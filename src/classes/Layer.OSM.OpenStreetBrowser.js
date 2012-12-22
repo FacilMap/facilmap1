9f6455c3aa24a7b6613808abfd6512a6e6c7dff0
Candid Dauth <cdauth@cdauth.eu>
2012-12-22 18:06:38 +0100

    Fixing OpenStreetBrowser layer

--- src/classes/Layer.OSM.OpenStreetBrowser.js
+++ src/classes/Layer.OSM.OpenStreetBrowser.js
@@ -24,7 +24,7 @@ FacilMap.Layer.OSM.OpenStreetBrowser = OpenLayers.Class(OpenLayers.Layer.OSM, {
 	numZoomLevels : 19,
 	attribution : OpenLayers.String.format(OpenLayers.i18n("attribution-osm"), { rendering: "<a href=\"http://www.openstreetbrowser.org/\">OpenStreetBrowser</a>" }),
 	initialize: function(name, options) {
-		OpenLayers.Layer.OSM.prototype.initialize.apply(this, [ name, "http://www.openstreetbrowser.org/tiles/base/${z}/${x}/${y}.png", options ]);
+		OpenLayers.Layer.OSM.prototype.initialize.apply(this, [ name, "http://tiles-base.openstreetbrowser.org/tiles/basemap_base/${z}/${x}/${y}.png", options ]);
 	},
 	CLASS_NAME : "FacilMap.Layer.OSM.OpenStreetBrowser"
 });
