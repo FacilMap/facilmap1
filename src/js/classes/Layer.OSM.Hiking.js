0f7370d92d23d0efa2c58b992bc53f42d6886223
Candid Dauth <cdauth@cdauth.eu>
2013-12-30 23:39:00 +0100

    Hiking symbols map changed URL

--- src/js/classes/Layer.OSM.Hiking.js
+++ src/js/classes/Layer.OSM.Hiking.js
@@ -27,7 +27,7 @@ FacilMap.Layer.OSM.Hiking = ol.Class(fm.Layer.OSM, {
 	isBaseLayer : false,
 	attribution : ol.i18n("attribution-hiking"),
 	initialize : function(name, options) {
-		fm.Layer.OSM.prototype.initialize.apply(this, [ name, "http://osm.lonvia.de/hiking/${z}/${x}/${y}.png", options ]);
+		fm.Layer.OSM.prototype.initialize.apply(this, [ name, "http://tile.waymarkedtrails.org/hiking/${z}/${x}/${y}.png", options ]);
 	},
 	CLASS_NAME : "FacilMap.Layer.OSM.Hiking"
 });
