a36802474b2a3ed22d51d10b5f6d186fe6eb8106
Candid Dauth <cdauth@cdauth.eu>
2015-12-19 00:51:13 +0100

    Make MapSurfer the default layer

--- src/js/classes/Map.js
+++ src/js/classes/Map.js
@@ -169,8 +169,8 @@ FacilMap.Map = ol.Class(ol.Map, {
 		var map = this;
 		var cat = ol.i18n("OpenStreetMap");
 
-		this.addLayer(new fm.Layer.OSM.Mapnik(ol.i18n("Mapnik"), { permalinkName : "Mpnk", category : cat }));
 		this.addLayer(new fm.Layer.OSM.MapSurfer.Road(ol.i18n("MapSurfer Road"), { permalinkName : "MSfR", category : cat }));
+		this.addLayer(new fm.Layer.OSM.Mapnik(ol.i18n("Mapnik"), { permalinkName : "Mpnk", category : cat }));
 		this.addLayer(new fm.Layer.OSM.OpenStreetBrowser(ol.i18n("OpenStreetBrowser"), { permalinkName : "OSBr", category : cat }));
 		this.addLayer(new fm.Layer.OSM.CycleMap(ol.i18n("OpenCycleMap"), { permalinkName : "OCyc", category : cat }));
 		//this.addLayer(new fm.Layer.OSM.Wanderkarte(ol.i18n("Reit- und Wanderkarte"), { permalinkName : "OSMC", category : cat }));
